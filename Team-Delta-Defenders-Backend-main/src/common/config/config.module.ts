32604d1b83725ee2c64fe5c147722063aef642d6
Femi Asefon <asefonfemi@gmail.com>
2025-09-24 09:05:49 +0100

    render deployment

--- Team-Delta-Defenders-Backend-main/src/common/config/config.module.ts
+++
@@ -1,9 +0,0 @@
-// common/config/config.module.ts
-import { Module } from '@nestjs/common';
-import { EnvironmentConfig } from './environment.config';
-
-@Module({
-  providers: [EnvironmentConfig],
-  exports: [EnvironmentConfig],
-})
-export class ConfigModule {}
--- Team-Delta-Defenders-Backend-main/src/common/decorators/current-user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, type ExecutionContext } from '@nestjs/common';
-import type { UserPayload } from '../../types/global.types';
-
-export const CurrentUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext): UserPayload => {
-    const request = ctx.switchToHttp().getRequest();
-    return request.user as UserPayload;
-  },
-);
